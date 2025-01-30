43c6e87e39efa9ad309c68067937ebb00d500f72
w69760 <w69760@student.wsiz.edu.pl>
2025-01-31 00:14:15 +0100

    Lab02

---
+++ Lab02/Osoba.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    public class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int wiek;
+
+        public Osoba(string imie, string nazwisko, int wiek)
+        {
+            if (IsValidImie(imie))
+            {
+                this.imie = imie;
+            }
+            else
+            {
+                throw new ArgumentException("Imię musi mieć co najmniej 2 znaki");
+            }
+
+            if (IsValidNazwisko(nazwisko))
+            {
+                this.nazwisko = nazwisko;
+            }
+            else
+            {
+                throw new ArgumentException("Nazwisko musi mieć co najmniej 2 znaki");
+            }
+            
+            if (IsValidWiek(wiek))
+            {
+                this.wiek = wiek;
+            }
+            else
+            {
+                throw new ArgumentException("Wiek musi być dodatni");
+            }    
+
+            
+        }
+        private bool IsValidImie(string imie)
+        {
+            return imie.Length >= 2;
+        }
+        private bool IsValidNazwisko(string nazwisko)
+        {
+            return nazwisko.Length >= 2;
+        }
+        private bool IsValidWiek(int wiek)
+        {
+            return wiek > 0;
+        }
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Imie: {imie}, nazwisko: {nazwisko}, wiek: {wiek}");
+        }
+    }
+}
---
+++ Lab02/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    public class Student
+    {
+        public string Imie { get;private set; }
+        public string Nazwisko { get;private set; }
+        private List<int> oceny;
+
+        public Student(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            this.oceny = new List<int>();
+        }
+        public double SredniaOcen
+        {
+            get
+            {
+                return oceny.Any() ? oceny.Average() : 0;
+            }
+        }
+        public void DodajOcene(int ocena)
+        {
+            if (ocena < 1 || ocena > 6)
+            {
+                throw new ArgumentException("Ocena musi być od 1 do 6");
+            }
+            
+            oceny.Add(ocena);
+        }
+    }
+}
