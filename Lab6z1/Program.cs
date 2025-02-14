bee7fcb95770cd3ab7c809a6347dda46c585f470
w69760 <w69760@student.wsiz.edu.pl>
2025-02-14 15:49:57 +0100

    lab 6z1

---
+++ Lab6z1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj nazwę pliku (bez rozszerzenia): ");
+        string nazwaPliku = Console.ReadLine() + ".txt";
+
+        Console.Write("Podaj numer albumu: ");
+        string nrAlbumu = Console.ReadLine();
+
+        try
+        {
+            File.WriteAllText(nazwaPliku, nrAlbumu);
+            Console.WriteLine($"Numer albumu {nrAlbumu} zapisano do pliku {nazwaPliku}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Błąd podczas zapisu do pliku: {e.Message}");
+        }
+    }
+}
